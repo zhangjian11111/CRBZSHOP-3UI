66f8ee4a654546897d7c89e20be17293bd5a9f09
zhangj <1097951193@qq.com>
2022-07-09 17:57:26 +0800

    初始化3个前端包括PC端的买家端，卖家后台，管理后台

---
+++ buyer/public/config.js
@@ -0,0 +1,17 @@
+var BASE = {
+    /**
+     * @description api请求基础路径
+     */
+    API_DEV: {
+      common: "http://localhost:8890",
+      buyer: "http://localhost:8888",
+      seller: "http://localhost:8855",
+      manager: "http://localhost:8866"
+    },
+    API_PROD: {
+      common: "http://localhost:8890",
+      buyer: "http://localhost:8888",
+      seller: "http://localhost:8855",
+      manager: "http://localhost:8866"
+    },
+  };
---
+++ manager/public/config.js
@@ -0,0 +1,48 @@
+var BASE = {
+    /**
+     * @description api请求基础路径
+     */
+    API_DEV: {
+      // common: "http://localhost:8890",
+      // buyer: "http://localhost:8888",
+      // seller: "http://localhost:8855",
+      // manager: "http://localhost:8866"
+      //  manager: "http://192.168.0.120:8887",
+      //  common: "http://192.168.0.120:8890",
+
+      // common: "https://yqswgjz.mynatapp.cc",
+      // buyer: "https://yqswgjz.mynatapp.cc",
+      // seller: "https://yqswgjz.mynatapp.cc",
+      // manager: "https://yqswgjz.mynatapp.cc"
+
+      common: "http://127.0.0.1:9111",
+      buyer: "http://127.0.0.1:9111",
+      seller: "http://127.0.0.1:9111",
+      manager: "http://127.0.0.1:9111"
+
+
+    },
+    API_PROD: {
+      // common: "http://localhost:8890",
+      // buyer: "http://localhost:8888",
+      // seller: "http://localhost:8855",
+      // manager: "http://localhost:8866"
+      common: "https://yqswgjz.mynatapp.cc",
+      buyer: "https://yqswgjz.mynatapp.cc",
+      seller: "https://yqswgjz.mynatapp.cc",
+      manager: "https://yqswgjz.mynatapp.cc"
+    },
+    /**
+     * @description // 跳转买家端地址 pc端
+     */
+    // PC_URL: "https://pc-b2b2c.pickmall.cn",
+    PC_URL: "https://yqswgjz.mynatapp.cc",
+    /**
+     * @description  // 跳转买家端地址 wap端
+     */
+    WAP_URL: "https://m-b2b2c.pickmall.cn",
+    /**
+     *  @description api请求基础路径前缀
+     */
+    PREFIX: "/manager"
+  };
