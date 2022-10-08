843c167e23b661ed0c9da9e5aac8f176ab721856
zhangj <1097951193@qq.com>
2022-10-08 22:40:45 +0800

    拼接sku的sn作为商品goods的sn

--- seller/src/views/goods/goods-seller/goodsOperationSec.vue
+++ seller/src/views/goods/goods-seller/goodsOperationSec.vue
@@ -1775,7 +1775,7 @@ export default {
           let skuInfoNames = this.skuInfo.map((n) => n.name);
           submit.skuList = [];
           this.skuTableData.map((sku) => {
-            submit.sn = sku.sn;
+            submit.sn += sku.sn;
             let skuCopy = {
               cost: sku.cost,
               price: sku.price,
