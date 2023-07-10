3f19f59f2d8a9cd0024f0fcbac463e52122e1bc2
zhangj <1097951193@qq.com>
2023-07-10 22:23:33 +0800

    修改dev和prod请求地址

--- buyer/public/config.js
+++ buyer/public/config.js
@@ -3,15 +3,15 @@ var BASE = {
      * @description api请求基础路径
      */
     API_DEV: {
-      // common: "http://127.0.0.1:9111",
-      // buyer: "http://127.0.0.1:9111",
-      // seller: "http://127.0.0.1:9111",
-      // manager: "http://127.0.0.1:9111"
+      common: "http://127.0.0.1:9111",
+      buyer: "http://127.0.0.1:9111",
+      seller: "http://127.0.0.1:9111",
+      manager: "http://127.0.0.1:9111"
 
-      common: "https://yqswgjz.mynatapp.cc",
-      buyer: "https://yqswgjz.mynatapp.cc",
-      seller: "https://yqswgjz.mynatapp.cc",
-      manager: "https://yqswgjz.mynatapp.cc"
+      // common: "https://yqswgjz.mynatapp.cc",
+      // buyer: "https://yqswgjz.mynatapp.cc",
+      // seller: "https://yqswgjz.mynatapp.cc",
+      // manager: "https://yqswgjz.mynatapp.cc"
     },
     API_PROD: {
       // common: "https://yqswgjz.mynatapp.cc",
@@ -19,9 +19,9 @@ var BASE = {
       // seller: "https://yqswgjz.mynatapp.cc",
       // manager: "https://yqswgjz.mynatapp.cc"
 
-      common: "https://www.qgxbm.com",
-      buyer: "https://www.qgxbm.com",
-      seller: "https://www.qgxbm.com",
-      manager: "https://www.qgxbm.com"
+      common: "https://common-api.qgxbm.com",
+      buyer: "https://buyer-api.qgxbm.com",
+      seller: "https://seller-api.qgxbm.com",
+      manager: "https://admin-api.qgxbm.com"
     },
   };
