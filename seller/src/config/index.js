d6f763129e9895b64106c7a45192b6220599ca4c
zhangj <1097951193@qq.com>
2023-08-25 15:18:48 +0800

    修改文章页面红线突兀样式 商家端使用最新地址选择器，默认在config关闭高德地图功能，开启后可继续使用

--- seller/src/config/index.js
+++ seller/src/config/index.js
@@ -6,9 +6,10 @@ module.exports = {
    * 添加成功后，可获取到key值和安全密钥jscode（自2021年12月02日升级，升级之后所申请的 key 必须配备安全密钥 jscode 一起使用)
    */
   //FIXME 请检查当前高德key创建的日期，如果2021年12月02日之前申请的 无需填写安全密钥
-   aMapSecurityJsCode:"0e74d5d3f1cdf049f6129e7f6f02b496", // 高德web端js申请的安全密钥
+  aMapSecurityJsCode:"0e74d5d3f1cdf049f6129e7f6f02b496", // 高德web端js申请的安全密钥
   aMapKey: "680dd24a763ecab260932ec8099b5c10", //高德web端js申请的api key
   website: "https://www.pickmall.cn", //官网地址
+  aMapSwitch:false, //是否开启高德定位
   enableCDN: true, //生产环境 是否启用cdn加载 vue等js
   port: 10002, //端口
   inputMaxLength: "2000", //全局输入框默认最大输入长度字
